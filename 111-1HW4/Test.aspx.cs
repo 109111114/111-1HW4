2eb27495dee4ee20fbbd20f600da4976dc0d7ed3
109111114 <a0921567518@gmail.com>
2022-12-17 15:18:08 +0800

    完成3

--- 111-1HW4/Test.aspx.cs
+++ 111-1HW4/Test.aspx.cs
@@ -33,8 +33,8 @@ namespace _111_1HW4
                 SqlDataAdapter o_A = new SqlDataAdapter("Select * from Users", o_Conn);
                 DataSet o_D = new DataSet();
                 o_A.Fill(o_D, "ds_Res");
-                gv_DataShow.DataSource = o_D;
-                gv_DataShow.DataBind();
+                gd_View.DataSource = o_D;
+                gd_View.DataBind();
                 o_Conn.Close();
             }
             catch (Exception o_Exc)
